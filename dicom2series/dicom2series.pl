9a0757b580feadb2d05555967d58d8079ff30a11
Philip Cook <cookpa@mail.med.upenn.edu>
2014-08-11 16:14:45 -0400

    ENH: Add series date and time info to output
    ENH: Log errors to stderr - only final report to stdout

--- dicom2series/dicom2series.pl
+++ dicom2series/dicom2series.pl
@@ -5,13 +5,15 @@
 
 use strict;
 use File::Basename;
-use File::Path;
+use File::Compare;
 use File::Copy;
+use File::Find;
+use File::Path;
 use FindBin qw($Bin);
 
 
 my $usage = qq {
-  Usage: dicom2series <output_dir> <empty_fields> <rename_files> <dicom_file1> ... <dicomfileN>
+  Usage: dicom2series <output_dir> <empty_fields> <rename_files> <dicom_directory> 
 
     <output_dir> - output base directory, which should be equal to the subject's unique identifier
  
@@ -20,29 +22,23 @@ my $usage = qq {
       not named "anonymize" because there are multiple definitions of what it means to anonymize a dicom header. You should check
       the config file and add fields that should be removed in order to protect subject confidentiality. 
 
-      Private fields cannot be anonymized by gdcmanon. Some PACS systems will copy patient information into private fields where
-      they can't be touched. Always check the output to ensure that the anonymization completed successfully.
+      Private fields cannot be altered by gdcmanon. Some PACS systems will copy patient information into private fields where
+      they can't be touched. Always check the output to ensure that the required fields were removed successfully.
 
     <rename_files> - 1 if you want to rename files (if possible) in the output directory, 0 otherwise.
 
- 
+    <dicom_directory> - A directory that will be searched recursively for dicom files.
+
     dicom2series requires that all input DICOM files pertain to the same subject. That is, you need to call dicom2series
     separately for each subject's data. It is not necessary to separate data from the same subject by time point; that will
     be done automatically. If the input contains both DICOM and non DICOM files, the non DICOM files will be ignored.
-      
+ 
 
     Example: 
 
-      dicom2series.sh /home/user/data/subjectsDICOM/subject 1 0 /home/user/data/raw/subject/dicom/*
+      dicom2series.sh /home/user/data/subjectsDICOM/subject 1 0 /home/user/data/raw/subject/dicom/
 
     White space and special characters in the series / file names will be removed or replaced with underscores.
-
-    Because of Unix command line limits, you may need xargs to process large numbers of files:
-
-      find -L <input_dir> -type f -print0 | xargs -0 dicom2series.sh <output_dir> <empty_fields> <rename_files>
-
-    where input_dir contains DICOM files for this particular subject only.
-
 
     Output will be sorted into separate directories named by acquisition date and time.  
     Within each time point scans will be named according to the series number, protocol name, and series description.
@@ -58,11 +54,11 @@ if (!($#ARGV + 1)) {
     print "$usage\n";
     exit 0;
 }
-elsif ($#ARGV < 4) {
+elsif ($#ARGV < 3) {
     die "ERROR: Missing arguments, run without args to see usage\n\t";
 }
 
-my ($outputDir, $anonymize, $renameFiles, @dicomFiles) = @ARGV;
+my ($outputDir, $anonymize, $renameFiles, $dicomDir) = @ARGV;
 
 my $gdcmDir = $ENV{'GDCMPATH'};
 
@@ -86,7 +82,7 @@ if ($anonymize) {
         my $field = $1;
 
         push @fields, $field;
-
+ 
         $anonString = $anonString . " --empty $field ";
       }
    }
@@ -96,68 +92,147 @@ if ($anonymize) {
 } 
 
 
-# Array of files that cannot be correctly converted, typically due to missing header information
-my @problemFiles;
+# Array of DICOM files that cannot be correctly converted, typically due to missing header information
+my @problemFiles = ();
 
+# Count how many dicom files were processed successfully for each series - this is often predictable for a particular acquisition
+# Series defined as date/series_dir_name
+my %dicomSeriesFileCounter = ();
 
-foreach my $dicomFile (@dicomFiles) {
+# Just in case there are problems, store output file in a hash that looks up the original file
+my %outputToInputMapping = ();
 
-    my ($isDicom, $missingInfo, $timepoint, $seriesDir, $newFileName) = getFileInfo($dicomFile, $renameFiles);
+# Potential dicom files, will need to check them all
+find({ wanted => \&processFile, no_chdir => 1, follow => 1}, $dicomDir);
 
-    # Make sure file really is dicom
-    if (!$isDicom) {
-	print "Skipping non DICOM file $dicomFile\n";
-	next;
-    }
+print "\n\nFinished searching $dicomDir\n\nDICOM data manifest:\n\n";
 
-    if ($missingInfo) {
-	push(@problemFiles, $dicomFile);
-	print "  WARNING: Insufficient header information to process $dicomFile - missing series description, series number or acquisition date\n";
-	next;
-    }
-    
+print "Date,seriesNumber,seriesName,DicomFiles\n";
 
-    if ( ! -d "${outputDir}/${timepoint}/${seriesDir}" ) {
-	mkpath("${outputDir}/${timepoint}/${seriesDir}") or die "  Cannot create series directory ${outputDir}/${seriesDir}";
-    }
+foreach my $seriesKey (sort(keys(%dicomSeriesFileCounter))) {
 
-    if ( -f "${outputDir}/${timepoint}/${seriesDir}/${newFileName}" ) {
-	print "  WARNING: Multiple files map to ${outputDir}/${timepoint}/${seriesDir}/${newFileName}\n";
+    my $seriesDataCSV = $seriesKey;
 
-	push(@problemFiles, $dicomFile);
-    }
-    else {
-	print "$dicomFile -> ${outputDir}/${timepoint}/${seriesDir}/${newFileName}\n";
-	
-        if ($anonymize) {
-	    my $notOK = system("${gdcmDir}/gdcmanon -i \"$dicomFile\" -o ${outputDir}/${timepoint}/${seriesDir}/${newFileName} $anonString"); 
-            
-            if ($notOK) {
-              die "gdcmanon returned non-zero exit code - fields may not have been correctly emptied\n";
-            }
-	}
-	else {
-	    copy($dicomFile, "${outputDir}/${timepoint}/${seriesDir}/${newFileName}") or die "Cannot copy files";
-	}
-    }
+    # Map date/0000_someSeries to date,0000,someSeries
+    $seriesDataCSV =~ s|/(\d\d\d\d)_|,${1},|g;
 
+    print "$seriesDataCSV,$dicomSeriesFileCounter{$seriesKey}\n";
 }
+
+print "\n";
+ 
 
 if ($#problemFiles > -1) {
 
-    print "\nThe following dicom files could not be processed:\n";
+    print "\nThe files below could not be processed successfully.\n\nError key:\n";
+    print "  DUPLICATE_INPUT : Multiple copies of the same file are present within the input directory. The first one encountered is processed.\n"; 
+    print "  MISSING_HEADER_INFO : Could not extract sufficient header information to map file to output. File not processed.\n"; 
+    print "  OUTPUT_NAMING_COLLISION : Different files map to the same output. Conflicting files moved to a separate output directory. These conflicts must be resolved manually.\n"; 
+    print "\nFile,Error\n";
 
     foreach my $problemFile (@problemFiles) {
-	print "  $problemFile\n";
+	print "$problemFile\n";
     }
-
-    print "\n  WARNING: Some dicom files could not be processed, most likely due to missing header information\n\n";
     
 }
 
 
 #
-# (isDICOM, missingInfo, acquisitionDate, seriesDir, newFileName) = getFileInfo($dcmFile, $renameFiles)
+# processFile($aFile) deals with a potential dicom file. Called by find
+#
+sub processFile {
+
+    my $aFile = $_;
+
+    # First verify that it's really a regular file
+    if (! -f $aFile) {
+        return;
+    }
+
+    my ($isDicom, $missingInfo, $timepoint, $seriesDir, $newFileName) = getFileInfo($aFile, $renameFiles);
+
+    # Make sure file really is dicom
+    if (!$isDicom) {
+	return;
+    }
+
+    my $dicomFile = $aFile;
+
+    if ($missingInfo) {
+	push(@problemFiles, "${dicomFile},MISSING_HEADER_INFO");
+	print STDERR "  ERROR: Insufficient header information to process $dicomFile - missing series description, series number or acquisition date\n";
+	return;
+    }
+    
+
+    if ( ! -d "${outputDir}/${timepoint}/${seriesDir}" ) {
+	mkpath("${outputDir}/${timepoint}/${seriesDir}") or die "  Cannot create series directory ${outputDir}/${seriesDir}";
+	print "Creating series directory ${outputDir}/${timepoint}/${seriesDir}\n";
+    }
+
+    my $newFileWithPath = "${outputDir}/${timepoint}/${seriesDir}/${newFileName}";
+    
+    if ( exists $outputToInputMapping{$newFileWithPath} ) {
+	print STDERR "  ERROR: Multiple files map to ${newFileWithPath}\n";
+        
+        # Compare to unanonymized "original" file, ie the one processed first which may or may not be the one the user wants
+        my $originalOrIsIt = $outputToInputMapping{$newFileWithPath};
+        
+        if ( compare($originalOrIsIt,$dicomFile) == 0 ) {
+            print STDERR "    Skipping $dicomFile : identical to $originalOrIsIt\n";
+            push(@problemFiles, "${dicomFile},DUPLICATE_INPUT");
+            return; 
+        }
+        else {
+            
+            # Files map to the same name, but are different - a more serious problem
+            
+            print STDERR "\n    ERROR: Different files map to the same output.\n    $dicomFile -> $newFileWithPath\n    conflicts with previous mapping\n    $originalOrIsIt -> $newFileWithPath\n\n";
+            push(@problemFiles, "${dicomFile},OUTPUT_NAMING_COLLISION");
+
+            # Dump the imposters to another directory
+            # Rename series dir so these get counted separately below
+            $seriesDir = "${seriesDir}_NamingConflicts";
+
+            my $outputDirCollision = "${outputDir}/${timepoint}/${seriesDir}";
+
+            if (! -d $outputDirCollision ) {
+                mkpath($outputDirCollision) or die "  Cannot create series directory $outputDirCollision";
+            }
+
+            # Worst case scenario, there is multiple degeneracy in file names
+            my $collisionCounter=1;
+
+            $newFileWithPath = "${outputDirCollision}/conflict_${collisionCounter}_${newFileName}";
+
+            while ( exists $outputToInputMapping{$newFileWithPath} ) {
+                $collisionCounter++;
+                $newFileWithPath = "${outputDirCollision}/conflict_${collisionCounter}_${newFileName}";
+            }
+        }
+    }
+
+    if ($anonymize) {
+        my $notOK = system("${gdcmDir}/gdcmanon -i \"$dicomFile\" -o $newFileWithPath"); 
+        
+        if ($notOK) {
+            # Die immediately rather than continue with information not removed 
+            die "gdcmanon returned non-zero exit code - fields may not have been correctly emptied\n";
+        }
+    }
+    else {
+        copy($dicomFile, "$newFileWithPath") or die "Cannot copy files";
+    }
+    
+    $outputToInputMapping{$newFileWithPath} = $dicomFile;
+    $dicomSeriesFileCounter{"${timepoint}/${seriesDir}"}++;
+    
+
+}
+
+
+#
+# (isDICOM, missingInfo, studyDate, seriesDir, newFileName) = getFileInfo($dcmFile, $renameFiles)
 #
 # If file is not DICOM, return an array of zeros.
 #
@@ -205,28 +280,16 @@ sub getFileInfo {
 
     $header =~ m/\s*\(0008,0020\) (?:\?\? \()?DA\)? \[(\d+)/ or return @missingInfo;
 
-    my $acquisitionDate = $1;
+    my $studyDate = $1;
     
     # Add underscores for readability goodness
-    $acquisitionDate =~ s/(\d{4})(\d{2})(\d{2})/${1}_${2}_${3}/;
+    $studyDate =~ s/(\d{4})(\d{2})(\d{2})/${1}_${2}_${3}/;
 
     # Add time
     $header =~ m/\s*\(0008,0030\) (?:\?\? \()?TM\)? \[(\d{4})/ or return @missingInfo;
 
-    $acquisitionDate = "${acquisitionDate}_$1";
+    $studyDate = "${studyDate}_$1";
 
-    # Some headers have missing instance numbers, as a fallback use original file name
-    my $acquisitionNumber = "";
-    
-    if ( $header =~ m/\s*\(0020,0013\) (?:\?\? \()?IS\)? \[(\d+)/ ) {
-	$acquisitionNumber = $1;
-	$acquisitionNumber = sprintf("%.4d", $acquisitionNumber);
-    }
-    else {
-	# Don't complain about this, but do not rename file if we don't have an instance number.
-	$renameFiles = 0;
-    }
-    
     $header =~ m/\s*\(0020,0011\) (?:\?\? \()?IS\)? \[(\d+)/ or return @missingInfo;
 
     my $seriesNumber = $1;
@@ -270,47 +333,80 @@ sub getFileInfo {
 
     if (!($seriesDescription eq $protocolName)) {
   
-      my $extraDescription = "";
-
-      if ($seriesDescription =~ m/${protocolName}/) {
-        # If protocol name is substring of series description, don't repeat protocol name
-        # Often the case for DTI
-        $extraDescription = $seriesDescription;
-
-        $extraDescription =~ s/${protocolName}//;
-
-        # Clean up trailing or leading _
-        $extraDescription =~ s/^_//;
-
-        $extraDescription =~ s/_$//;
-
-        # In case protocol name is in the middle
-        $extraDescription =~ s/__/_/g;
-
-
+        my $extraDescription = "";
         
+        if ($seriesDescription =~ m/${protocolName}/) {
+            # If protocol name is substring of series description, don't repeat protocol name
+            # Often the case for DTI
+            $extraDescription = $seriesDescription;
+            
+            $extraDescription =~ s/${protocolName}//;
+            
+            # Clean up trailing or leading _
+            $extraDescription =~ s/^_//;
+            
+            $extraDescription =~ s/_$//;
+
+            # In case protocol name is in the middle
+            $extraDescription =~ s/__/_/g;
+        }
+        elsif ($protocolName =~ m/${seriesDescription}/) {
+            # If series description is a substring of protocol name, don't repeat it
+            # nothing to add in this case
+            $extraDescription = "";
+        } 
+        else {
+            # Distinct protocol name and description, append both
+            $extraDescription = $seriesDescription; 
+        }
         
-     }
-     elsif ($protocolName =~ m/${seriesDescription}/) {
-       # If series description is a substring of protocol name, don't repeat it
-       # nothing to add in this case
-       $extraDescription = "";
-     } 
-     else {
-       # Distinct protocol name and description, append both
-       $extraDescription = $seriesDescription; 
-     }
-
-     if ($extraDescription) {
-       $seriesDir = "${seriesDir}_${extraDescription}"; 
-     }
-   }
-
+        if ($extraDescription) {
+            $seriesDir = "${seriesDir}_${extraDescription}"; 
+        }
+    }
+    
     my $newFileName = $fileBaseName . $fileExtension;
     
     if ( $renameFiles ) {
-	$newFileName = join('_', $acquisitionNumber, $seriesNumber, $protocolName);
-	
+
+        # Need some extra information to rename files
+
+        # Name files {instance number}_{series number}_{protocol name}_{series date}_{series time}[.gz if data compressed]
+        # 
+        # vbrename does {instance number}_{series date}_{series time} - 
+        # 
+        my $instanceNumber = "";
+        my $seriesDate = "";
+        my $seriesTime = "";
+        
+        if ( $header =~ m/\s*\(0020,0013\) (?:\?\? \()?IS\)? \[(\d+)/ ) {
+            $instanceNumber = $1;
+            $instanceNumber = sprintf("%.4d", $instanceNumber);
+        }
+        else {
+            print "Can't rename file - missing data in field (0020,0013)\n";
+            return (0, 0, 0, 0, 0); 
+        }
+    
+        if ( $header =~ m/\s*\(0008,0021\) (?:\?\? \()?DA\)? \[(\d+)/ ) {
+            $seriesDate = $1;
+        }
+        else {
+            print "Can't rename file - missing data in field (008,0021)\n";
+            return (0, 0, 0, 0, 0); 
+        }
+
+        if ( $header =~ m/\s*\(0008,0031\) (?:\?\? \()?TM\)? \[([0-9.]+)/ ) {
+            $seriesTime = $1;
+        }
+        else {
+            print "Can't rename file - missing data in field (008,0031)\n";
+            return (0, 0, 0, 0, 0); 
+        }
+        
+
+	$newFileName = join('_', $instanceNumber, $seriesNumber, $protocolName, $seriesDate, $seriesTime);
+
 	# Preserve file extension if it indicates compression
 	if ( $fileExtension =~ m/gz/i ) {
 	    $newFileName = $newFileName . $fileExtension;
@@ -329,7 +425,7 @@ sub getFileInfo {
     # Finally, clear up multiple underscores
     $newFileName =~ s/_+/_/g;
 
-    return (${isDicom}, 0, ${acquisitionDate}, ${seriesDir}, ${newFileName});
+    return (${isDicom}, 0, ${studyDate}, ${seriesDir}, ${newFileName});
     
 }
 
